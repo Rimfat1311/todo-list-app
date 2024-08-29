50998902f0f3044a30f8018a7ead729ca3d452f5
rimfat1311 <simontimnan@gmail.com>
2024-08-29 06:40:56 -0700

    update

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,21 +1,47 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import check from '../assets/icons/check.png'
 import deleteIcon from '../assets/icons/delete.png'
 
-const ListItems = ({text, id, isComplete, deleteTodo}) => {
+const ListItems = ({text, id, isComplete, deleteTodo, toggle}) => {
 
-    
+    const [change, setChange] =useState(false)
+    const [istext, setText] =useState(text)
+    const handleChange =()=>{
+        setChange(true)
+    }
+    const handleClick =(e)=>{
+       setText(e.target.value)
+    }
 
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          setChange(false);
+        }
+      };
+    // const handleBlur = () => {
+    //     setIsEditing(false);
+    //   };
 
 
     return (
         <div className='flex items-center my-3 gap-2'>
-            <div className='flex flex-1 cursor-pointer items-center'>
+            <div onClick={()=>{Toggle(id)}} className='flex flex-1 cursor-pointer items-center'>
                 <img className='w-7' src={check} alt="" />
-
-                <p className='text-slate-700 ml-4 font-[17px]'>
-                    {text}
+                {change ? (
+        <input
+          type='text'
+          value={istext}
+          onChange={handleClick}
+          onKeyDown={handleKeyDown}
+        //   onBlur={handleBlur}
+          autoFocus
+          className='border p-1'
+        />
+      ) : (
+                <p onClick={handleChange} className='text-slate-700 ml-4 font-[17px]'>
+              {istext}
                 </p>
+                 )}
             </div>
 
             <img onClick={()=>{deleteTodo(id)}} className='w-4 cursor-pointer' src={deleteIcon} alt="" />
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ListItems from './ListItems';
-import img from '../assets/images/img.png'
+import img from '../assets/images/img.png';
+
 const TodoList = () => {
-  const [TodoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([]);
   const inputRef = useRef();
 
   const add = () => {
@@ -22,18 +23,28 @@ const TodoList = () => {
     inputRef.current.value = "";
   };
 
-  const deleteTodo = (id)=>{
-    setTodoList((prvTodo)=>{
-     return prvTodo.filter((todo)=> todo.id !== id)
-    })
- 
-  }
+  const deleteTodo = (id) => {
+    setTodoList((prevTodo) => prevTodo.filter((todo) => todo.id !== id));
+  };
+
+  const toggle = (id) => {
+    setTodoList((prevTodo) =>
+      prevTodo.map((todo) =>
+        todo.id === id
+          ? { ...todo, isComplete: !todo.isComplete }
+          : todo
+      )
+    );
+  };
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
 
   return (
     <div className='bg-slate-300 place-self-center w-1/3 h-[500px] flex flex-col text-slate-700 rounded-xl p-7'>
       <div className='flex items-center m-7 gap-2'>
-        {/* Add src to img or remove it if not needed */}
-        <img className='w-7' src={img} alt="" />
+        <img className='w-7' src={img} alt="ToDo list icon" />
         <h1 className='text-4xl font-bold'>ToDo-list</h1>
       </div>
       <div className='flex items-center my-7 bg-gray-200 rounded-full'>
@@ -51,8 +62,15 @@ const TodoList = () => {
         </button>
       </div>
       <div>
-        {TodoList.map((item) => (
-          <ListItems key={item.id} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} />
+        {todoList.map((item) => (
+          <ListItems
+            key={item.id}
+            text={item.text}
+            id={item.id}
+            isComplete={item.isComplete}
+            deleteTodo={deleteTodo}
+            toggle={toggle}
+          />
         ))}
       </div>
     </div>
