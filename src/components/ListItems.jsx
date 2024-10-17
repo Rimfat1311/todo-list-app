d32a2c37deb43fa9a293bfd9aaad984e96b8e82b
rimfat1311 <simontimnan@gmail.com>
2024-10-17 16:57:52 -0700

    update

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -2,49 +2,51 @@ import React, { useRef, useState } from 'react'
 import check from '../assets/icons/check.png'
 import deleteIcon from '../assets/icons/delete.png'
 
-const ListItems = ({ text, id, isComplete, deleteTodo, toggle }) => {
+const ListItems = ({text, id, isComplete, deleteTodo,  toggle}) => {
 
-  const [change, setChange] = useState(false)
-  const [istext, setText] = useState(text)
-  const handleChange = () => {
-    setChange(true)
-  }
-  const handleClick = (e) => {
-    setText(e.target.value)
-  }
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      setChange(false);
+    const [change, setChange] =useState(false)
+    const [istext, setText] =useState(text)
+    const handleChange =()=>{
+        setChange(true)
     }
-  };
-  
+    const handleClick =(e)=>{
+       setText(e.target.value)
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          setChange(false);
+        }
+      };
+    // const handleBlur = () => {
+    //     setIsEditing(false);
+    //   };
 
 
-  return (
-    <div className='flex items-center my-3 gap-2'>
-      <div onClick={() => { Toggle(id) }} className='flex flex-1 cursor-pointer items-center'>
-        <img className='w-7' src={check} alt="" />
-        {change ? (
-          <input
-            type='text'
-            value={istext}
-            onChange={handleClick}
-            onKeyDown={handleKeyDown}
-            //   onBlur={handleBlur}
-            autoFocus
-            className='border p-1'
-          />
-        ) : (
-          <p onClick={handleChange} className='text-slate-700 ml-4 font-[17px]'>
-            {istext}
-          </p>
-        )}
-      </div>
+    return (
+        <div className='flex items-center my-3 gap-2'>
+            <div onClick={()=>{Toggle(id)}} className='flex flex-1 cursor-pointer items-center'>
+                <img className='w-7' src={check} alt="" />
+                {change ? (
+        <input
+          type='text'
+          value={istext}
+          onChange={handleClick}
+          onKeyDown={handleKeyDown}
+        //   onBlur={handleBlur}
+          autoFocus
+          className='border p-1'
+        />
+      ) : (
+                <p onClick={handleChange} className='text-slate-700 ml-4 font-[17px]'>
+              {istext}
+                </p>
+                 )}
+            </div>
 
-      <img onClick={() => { deleteTodo(id) }} className='w-4 cursor-pointer' src={deleteIcon} alt="" />
-    </div>
-  )
+            <img onClick={()=>{deleteTodo(id)}} className='w-4 cursor-pointer' src={deleteIcon} alt="" />
+        </div>
+    )
 }
 
 export default ListItems
