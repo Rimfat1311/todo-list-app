43b67b474915827c8845948b496ef25a0d974da1
rimfat1311 <simontimnan@gmail.com>
2024-10-17 17:04:02 -0700

    update

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -42,27 +42,26 @@ const TodoList = () => {
   }, [todoList]);
 
   return (
-    <div className='bg-slate-300 place-self-center w-full md:w-1/3 h-[500px] flex flex-col text-slate-700 rounded-xl p-7'>
-      <div className='flex items-center m-7 gap-2'>
-        <img className='w-7' src={img} alt="ToDo list icon" />
-        <h1 className='text-4xl font-bold'>ToDo-list</h1>
+    <div className='bg-slate-300 mx-auto w-full sm:w-1/2 md:w-1/3 h-[500px] flex flex-col text-slate-700 rounded-xl p-5 sm:p-7'>
+      <div className='flex items-center mb-5 gap-2'>
+        <img className='w-8 h-8' src={img} alt="ToDo list icon" />
+        <h1 className='text-3xl sm:text-4xl font-bold'>ToDo-list</h1>
       </div>
-      <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+      <div className='flex items-center my-5 bg-gray-200 rounded-full'>
         <input
           ref={inputRef}
           className='bg-transparent border-0 outline-none flex-1 h-12 pl-6 pr-2 placeholder:text-slate-600'
           type="text"
-          placeholder='add to list'
+          placeholder='Add to list'
         />
         <button
           onClick={add}
-          className='border-none rounded-full bg-slate-700 text-white text-lg font-medium cursor-pointer w-32 h-12'
+          className='border-none rounded-full bg-slate-700 text-white text-lg font-medium cursor-pointer w-24 sm:w-32 h-12'
         >
-          add+
+          Add +
         </button>
       </div>
-      <div>
-      
+      <div className='flex-grow overflow-y-auto'>
         {todoList.map((item) => (
           <ListItems
             key={item.id}
